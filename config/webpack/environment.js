98258686113932ce6e231bd6219316a2c1a89006
Erika.K <koguchi.e915@gmail.com>
2024-11-27 15:18:14 +0900

    [Add] 11/27 Bootstrapを導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,3 +6,14 @@ environment.config.merge({
 });
 
 module.exports = environment;
+
+// Bootstrapの導入
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+  })
+)
