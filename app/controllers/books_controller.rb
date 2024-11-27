115a88c23de3cdbb209a53fbae0c823a4ff918a8
Erika.K <koguchi.e915@gmail.com>
2024-11-27 14:14:01 +0900

    [Add] 11/27 indexページ作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,37 @@
+class BooksController < ApplicationController
+  # 新規作成
+  def new
+    @books = Book.new
+  end
+
+  # データの保存
+  def create
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+  end
+
+  # 一覧画面表示
+  def index
+    # すべての本のデータを取得
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :image, :caption)
+  end
+end
