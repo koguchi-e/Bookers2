b8a369b4f6d32ee5431c4ff2d76c7bd0a441311b
Erika.K <koguchi.e915@gmail.com>
2024-11-29 11:47:28 +0900

    [Add] 11/28 サインアップ完成

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :redirect_if_logged_in, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]  # これを追加
 
   # GET /users/sign_up
   def new
@@ -8,6 +9,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   private
+
+  def configure_sign_up_params
+    # :email を追加
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+  end
 
   def redirect_if_logged_in
     # サインインしていない場合のみサインアップページを表示
