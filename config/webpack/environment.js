40c569ac2e4c944ef44538551b52fc3241bcd7b8
Erika.K <koguchi.e915@gmail.com>
2024-11-29 09:41:36 +0900

    [Add] 11/27 ログイン・サインアップ追加、ログウト完成

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,10 +5,8 @@ environment.config.merge({
   node: false
 });
 
-module.exports = environment;
-
 // Bootstrapの導入
-const webpack = require('webpack')
+const webpack = require('webpack');
 environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
@@ -16,4 +14,14 @@ environment.plugins.prepend(
     jQuery: 'jquery/src/jquery',
     Popper: 'popper.js'
   })
-)
+);
+
+// Sassの設定を追加
+const sassLoader = environment.loaders.get('sass');
+if (sassLoader) {
+  sassLoader.use.find(item => item.loader === 'sass-loader').options = {
+    additionalData: `@use 'sass:math';`
+  };
+}
+
+module.exports = environment;
