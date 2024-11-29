59aa208e27038d518d70951840a60b96a8ff8aaf
Erika.K <koguchi.e915@gmail.com>
2024-11-29 12:05:17 +0900

    [Add] 11/28 サインアップ完成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -16,3 +11,10 @@ import "../stylesheets/application";
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener('turbolinks:load', function () {
+  const nameInput = document.querySelector('input[name="user[name]"]');
+  if (nameInput) {
+    nameInput.focus();  // 名前入力欄にフォーカスを設定
+  }
+});
