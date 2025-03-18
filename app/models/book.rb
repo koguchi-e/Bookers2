e4d05326698aea48401f429ee2d8d076f970bad9
Erika.K <koguchi.e915@gmail.com>
2025-03-19 08:52:05 +0900

    [Update]通知機能・Bookモデル追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,7 @@ class Book < ApplicationRecord
   validates :title, presence: true, length: { in: 2..20 }, uniqueness: true
   validates :body, presence: true, length: { maximum: 200 }
   belongs_to :user
+
+  has_many :favorites, dependent: :destroy
+  has_many :notifications, as: :notifiable, dependent: :destroy
 end
