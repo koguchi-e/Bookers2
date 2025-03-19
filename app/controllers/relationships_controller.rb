8f7e64b608f3a1e36d4b9719b097b5f8054d0730
Erika.K <koguchi.e915@gmail.com>
2025-03-19 17:59:04 +0900

    [Update]フォロー/フォロワー機能・コントローラ修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,8 @@
 class RelationshipsController < ApplicationController
     def createt
+        user = User.find(params[:user_id])
+        current_user.follow(user)
+        redirect_to requesr.referer
     end
 
     def destroy
