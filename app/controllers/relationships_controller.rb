5ef9eeda71ae8df88753747077c6e8de230aa2c5
Erika.K <koguchi.e915@gmail.com>
2025-03-21 11:18:02 +0900

    [Upadate]フォロー機能・relationships_controller完成

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,9 +2,22 @@ class RelationshipsController < ApplicationController
     def createt
         user = User.find(params[:user_id])
         current_user.follow(user)
-        redirect_to requesr.referer
+        redirect_to request.referer
     end
 
     def destroy
+        user = User.find(params[:user_id])
+        current_user.unfollow(user)
+        redirect_to request.referer
+    end
+
+    def following
+        user = User.find(params[:user_id])
+        @users = User.following
+    end
+
+    def followers
+        user = user.find(params[:user_id])
+        @users = user.followers
     end
 end
