65bdef0c4bd587b8d5f29a4c9de55af24d548276
Erika.K <koguchi.e915@gmail.com>
2025-03-21 11:04:04 +0900

    [Upadate]フォロー機能・Userモデル完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,24 +6,40 @@ class User < ApplicationRecord
 
   self.authentication_keys = [:name]
 
-  validates :name, presence: true, length: { in: 2..20 }, uniqueness: true
-  validates :introduction, length: { maximum: 50 }
-
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
+
+  # 通知機能
   has_many :notifications, dependent: :destroy
 
-  has_many :reverse_of_relationships, class_name: "Relationship",
-  foreign_key: "followed_id", dependent: :destroy
-  
+  # フォロー機能
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+
+  validates :name, presence: true, length: { in: 2..20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
 
   def get_profile_image(width, height)
     if profile_image.attached?
       profile_image.variant(resize_to_limit: [width, height]).processed
     else
-      'default_profile_image.png' # デフォルト画像を指定
+      'default_profile_image.png'
     end
   end
 
+  def follow(user)
+    relationships.createt(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 end
